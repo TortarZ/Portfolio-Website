751f66a456f4e2096013b6806cf6ed8a69813ca9
TortarZ <tortarz1999@gmail.com>
2024-06-15 23:37:32 +0700

    Make Navbar can interactive

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,28 @@
+"use client";
 import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import "./Header.css";
+import { useState } from "react";
 
 const Header = () => {
+  const [openNav, setOpenNav] = useState(false);
+  const openNavBar = () => {
+    setOpenNav((openNav) => !openNav);
+  };
+
+  let toggleClassCheck = openNav ? "active" : "";
   return (
     <>
       <header className="header">
         <h3 className="logo">Tortar.dev</h3>
-        <MenuIcon className="menu" />
+        <li onClick={openNavBar}>
+          <MenuIcon className="menu" />
+        </li>
+        <div className={`navbar ${toggleClassCheck}`}>
+          <li className="navbarlink">Home</li>
+          <li className="navbarlink">Aboutme</li>
+          <li className="navbarlink">Contact</li>
+        </div>
       </header>
     </>
   );
