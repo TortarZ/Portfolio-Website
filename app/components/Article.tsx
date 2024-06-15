a6dff327e8458727cb1c3ac7fd1020cc69b12602
TortarZ <tortarz1999@gmail.com>
2024-06-15 19:12:32 +0700

    Make incoming project section

--- app/components/Article.tsx
+++ app/components/Article.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Article = () => {
-  return <div>Article</div>;
+  return (
+    <>
+      <section className="w-[390px] h-[228px] bg-[#f9f9f9] flex justify-center items-center">
+        <h2 className="text-[26px] font-bold pt-[80px] pb-[80px] pr-[17px] pl-[17px] text-center">
+          New projects coming soon..
+        </h2>
+      </section>
+    </>
+  );
 };
 
 export default Article;
