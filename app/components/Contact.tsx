91f4a53a61f5c6e0ae7c35236e19c7035ef800a1
TortarZ <tortarz1999@gmail.com>
2024-06-15 19:51:02 +0700

    Make contact section

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import "./Contact.css";
+import MapIcon from "@mui/icons-material/Map";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <section className="contact-section">
+        <div className="contact-box">
+          <h3 className="text-[#147efb] font-bold text-[17px] mb-[10px]">
+            CONTACT
+          </h3>
+          <h2 className="text-[#2d2e32] font-bold text-[25px]">
+            Don't be shy! Hit me up!
+          </h2>
+        </div>
+        <div className="subcontact-box">
+          <div className="contact-detail">
+            <li className="icon-box">
+              <MapIcon className="icon" />
+            </li>
+            <h3 className="icon-name">Location</h3>
+            <p className="icon-detail">Pathumthani, Thailand</p>
+          </div>
+          <div className="contact-detail">
+            <li className="icon-box">
+              <MailOutlineIcon className="icon" />
+            </li>
+            <h3 className="icon-name">Mail</h3>
+            <p className="icon-detail">sirikornsutthapas@gmail.com</p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
